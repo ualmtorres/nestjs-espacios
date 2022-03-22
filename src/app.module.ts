07b903d9c509694917a203465d416d11b36906af
Manuel Torres Gil <mtorres@ual.es>
2022-03-22 23:02:12 +0100

    Code smells corregidos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,4 @@
+import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
